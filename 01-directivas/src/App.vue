fb696f2cb2378011e54a9c0fe3437d8d14324bf2
Deddy Ibañez <floriban@gmail.com>
2024-07-08 18:40:49 -0500

    Practicando vuejs

---
+++ 01-directivas/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const puntajeCrediticio = 800
+const arrayEmoji = ['🍔', '🥞', '🍳', '🥐', '🥯', '🥖', '🍛', '🍲']
+</script>
+
+<template>
+  <!-- V-IF -->
+  <p v-if="puntajeCrediticio >= 900">Excelente cliente 😊</p>
+  <p v-else-if="puntajeCrediticio < 900 && puntajeCrediticio >= 700">Cliente regular 😐</p>
+  <p v-else>Pésimo cliente 😭</p>
+
+  <!-- V-SHOW -->
+  <p v-show="puntajeCrediticio >= 900">Excelente cliente 😊</p>
+  <p v-show="puntajeCrediticio < 900 && puntajeCrediticio >= 700">Cliente regular 😐</p>
+  <p v-show="puntajeCrediticio < 700">Pésimo cliente 😭</p>
+
+  <!-- V-FOR -->
+  <ul>
+    <li v-for="(item, indice) in arrayEmoji" :key="indice">
+      Emoji {{ item }}
+    </li>
+  </ul>
+
+</template>
+
+<style scoped></style>
