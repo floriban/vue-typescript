347f7b959db43d4cf31f7930ba9c3e03035c4639
Deddy Ibañez <floriban@gmail.com>
2024-07-08 18:41:42 -0500

    Practicando vuejs

---
+++ 00-hola-mundo/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const saludo: string = 'Hola a todos'
+const botonHtml = "<button>Enviar</button>"
+const color = "color: red"
+const web = 'https://www.google.com'
+const ahorro = 350
+</script>
+
+<template>
+  <h1>Como están</h1>
+  <h2>{{ saludo }}</h2>
+
+  <span v-html="botonHtml"></span>
+
+  <p :style="color">TEST</p>
+  <a :href="web">Ir al sitio</a>
+
+  <p :style="`color: ${ahorro > 100 ? 'green' : 'red'}`">{{ ahorro }}</p>
+  <p :class="ahorro > 100 ? 'alto' : 'bajo'">{{ ahorro }}</p>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 40px;
+}
+
+.bajo {
+  color: red;
+}
+
+.alto {
+  color: green;
+}
+</style>
